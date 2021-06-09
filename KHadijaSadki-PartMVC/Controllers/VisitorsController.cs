5933d340fa07c11352e2789aefcdc0002f70b95b
Sadki Khadija <Sadki.Khadija@student.intecbrussel.be>
2021-06-09 15:26:42 +0200

    Add project files.

---
+++ KHadijaSadki-PartMVC/Controllers/VisitorsController.cs
@@ -0,0 +1,63 @@
+﻿using KHadijaSadki_PartMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KHadijaSadki_PartMVC.Controllers
+{
+    public class VisitorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TempData["Visitors"] = 0;
+            TempData["Family Visitors"] = 0;
+            TempData["Oldest "] = 0;
+            TempData["Youngest"] = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input(Visitor visitor)
+        {
+            TempData["Visitors"] = (Int32)TempData.Peek("Visitors") + 1;
+            if (visitor.IsFamily)
+            {
+                TempData["FamilyMembers"] = (Int32)TempData.Peek("FamilyMembers") + 1;
+            }
+            if ((Int32)TempData.Peek("Youngest") == 0)
+            {
+                TempData["Youngest"] = visitor.Age;
+                TempData["Oldest"] = visitor.Age;
+            }
+            else
+            {
+                if ((Int32)TempData.Peek("Youngest") >= visitor.Age)
+                {
+                    TempData["Youngest"] = visitor.Age;
+                }
+                if ((Int32)TempData.Peek("Oldest") <= visitor.Age)
+                {
+                    TempData["Oldest"] = visitor.Age;
+                }
+            }
+
+            TempData.Keep();
+            return View(visitor);
+        }
+
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            ViewBag.TotalVisitors = TempData["Visitors"];
+            ViewBag.TotalFamilyMembers = TempData["FamilyMembers"];
+            ViewBag.OldestVisitor = TempData["Oldest"];
+            ViewBag.YoungestVisitor = TempData["Youngest"];
+            return View();
+        }
+    }
+}
+
---
+++ KHadijaSadki-PartMVC/Models/Visitor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KHadijaSadki_PartMVC.Models
+{
+    public class Visitor
+    {
+        [Required(ErrorMessage = "Enter your FirstName")]
+
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter your SecondName")]
+        public string SecondName { get; set; }
+        [EmailAddress(ErrorMessage = "Please enter valid email format")]
+        public string Email { get; set; }
+        [Required]
+        public string Tel { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Please enter your Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please enter your postcode tussen 1000-1500")]
+        [Range(1000, 1500)]
+        public int PostalCode { get; set; }
+        [Required(ErrorMessage = "Enter your City")]
+        public string City { get; set; }
+        [Required]
+        public bool IsFamily { get; set; }
+    }
+}
